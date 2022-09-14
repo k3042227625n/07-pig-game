11cde3f332685f786f3733d527f823573d1eb1c6
{km} <{k3042227625n@gmail.com}>
2022-09-15 07:43:49 +0900

    second commit

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
 'use strict';
 
+const player0El = document.querySelector('.player--0');
+const player1El = document.querySelector('.player--1');
 const score0EL = document.getElementById('score--0');
 const score1EL = document.getElementById('score--1');
 const current0EL = document.getElementById('current--0');
@@ -14,7 +16,9 @@ score0EL.textContent = 0;
 score1EL.textContent = 0;
 // diceEL.classList.add('hidden');
 
+const scores = [0, 0];
 let currentScore = 0;
+let activePlayer = 0;
 
 btnRoll.addEventListener('click', function() {
     const dice = Math.trunc(Math.random() * 6) + 1;
@@ -24,8 +28,16 @@ btnRoll.addEventListener('click', function() {
     
     if (dice !== 1) {
         currentScore += dice;
-        current0EL.textContent = currentScore;
+        document.getElementById(`current--${activePlayer}`).textContent = currentScore;
+        // current0EL.textContent = currentScore;
     }else {
+        // 次のプレイヤーに交代(プレイヤー０であるかどうかチェック)
+        document.getElementById(`current--${activePlayer}`).textContent = 0;
+        // スコアを一旦０に戻す
+        currentScore = 0;
+        activePlayer = activePlayer === 0 ? 1 : 0;
+        player0El.classList.toggle('player--active');
+        player1El.classList.toggle('player--active');
         
     }
 });
